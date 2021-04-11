c90ad2bf9c4a32c3d89c9f55b4bf9bfccfb88b32
Angelina <73379750+AngelinaMeleshko@users.noreply.github.com>
2021-04-11 21:34:04 +0300

    Update knight.cpp

--- knight.cpp
+++ knight.cpp
@@ -10,3 +10,20 @@ Knight::Knight(int x, int y, bool color) : ChessPiece(x, y)
         this->setPixmap(QPixmap::fromImage(*blackImage).scaled(90,90));
     }
 }
+bool Knight::figureCanMove(const Cell * start, const Cell * end)
+{
+
+    
+
+        if((start->row-1 ==end->row  &&  start->column+ 2 ==end->column ) || (start->row - 1 ==end->row  && start->column - 2 ==end->column) ||
+                  (start->row + 1 == end->row && start->column + 2 == end->column) || (start->row + 1 == end->row && start->column - 2 ==end->column )||
+                  (start->row - 2 == end->row && start->column + 1 == end->column) || (start->row - 2 == end->row && start->column - 1 == end->column) ||
+                  (start->row + 2 == end->row && start->column + 1 == end->column) || (start->row + 2 == end->row && start->column - 1 == end->column))
+        {
+                      return true;
+            }
+        {
+                  return false;
+              }
+
+}
