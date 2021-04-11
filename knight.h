09715394337d58f2d0b04b33ff79ff5a2fa84430
Angelina <73379750+AngelinaMeleshko@users.noreply.github.com>
2021-04-11 21:32:14 +0300

    Update knight.h

--- knight.h
+++ knight.h
@@ -8,6 +8,7 @@ public:
     Knight(int x, int y, bool color);
     QImage *whiteImage = new QImage(":/images/knight-white.png");
     QImage *blackImage = new QImage(":/images/knight-black.png");
+     virtual bool figureCanMove(const Cell * start, const Cell * end)override;
     bool color;
 };
 
