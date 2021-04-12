308f7c5e2b91db2a6e97b43704cb1ac53288641b
lizavetik <82059213+lizavetik@users.noreply.github.com>
2021-04-12 22:35:53 +0300

    Юля, не злись, пожалуйста ^_^

--- king.cpp
+++ king.cpp
@@ -9,5 +9,10 @@ King::King(int x, int y, bool color) : ChessPiece(x, y, color)
         this->setPixmap(QPixmap::fromImage(*blackImage).scaled(90,90));
     }
 }
+    bool King::figureCanMove(const Cell *start, const Cell *end)
+{
+    if (pow(start->column-end->column, 2) + pow(start->row-end->row, 2) <= 2) return true;
+    return false;
+}
 
 //Мы лучшие!!!!!
